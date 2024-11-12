e22ca7cd9e9d8cd22d453d5e455b11cdefc8e95d
abox992 <abox992@gmail.com>
2024-11-12 14:59:27 -0600

    removing diagnostics from none-ls, only lsp should handle that

--- lua/adam/plugins/lsp.lua
+++ lua/adam/plugins/lsp.lua
@@ -1,18 +1,18 @@
 return {
 
-	'neovim/nvim-lspconfig',
-	dependencies = {
-		'williamboman/mason.nvim',
-		'williamboman/mason-lspconfig.nvim',
-	},
-	config = function()
-		require('mason').setup()
-		require('mason-lspconfig').setup {
-			ensure_installed = {
-				'lua_ls',
-				'clangd',
-				'ts_ls',
-                'eslint',
+    'neovim/nvim-lspconfig',
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        "j-hui/fidget.nvim",
+    },
+    config = function()
+        require('mason').setup()
+        require('mason-lspconfig').setup {
+            ensure_installed = {
+                'lua_ls',
+                'clangd',
+                'ts_ls',
             },
             handlers = {
                 function(server_name)
@@ -38,5 +38,11 @@ return {
                 '--fallback-style=WebKit',
             },
         }
+
+        vim.diagnostic.config({
+            update_in_insert = true,
+        })
+
+        require('fidget').setup({})
     end,
 }
--- lua/adam/plugins/none-ls.lua
+++ lua/adam/plugins/none-ls.lua
@@ -10,9 +10,9 @@ return {
         null_ls.setup {
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.completion.spell,
+                -- null_ls.builtins.completion.spell,
                 null_ls.builtins.formatting.prettierd,
-                require("none-ls.diagnostics.eslint_d"),
+                -- require("none-ls.diagnostics.eslint_d"),
             },
         }
 
